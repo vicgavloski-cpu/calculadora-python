d5f6b6e42ab1c5aa966ac3ff63864f1216b58c0c
vicgavloski-cpu <vicgavloski@gmail.com>
2026-08-23 18:43:46 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+soma = n1 + n2
+print("A soma é:", soma)
+
+subtracao = n1 - n2
+print("A subtração é:", subtracao)
+
+divisao = n1 / n2
+print("A divisão é:", divisao)
+
+multiplicacao = n1 * n2
+print("A multiplicação é:", multiplicacao)
